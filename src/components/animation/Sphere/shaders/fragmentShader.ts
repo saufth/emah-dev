fdf7473c29107132daa9fa89465789af6da460b1
saufth <saufth@gmail.com>
2023-01-31 13:21:41 -0600

    Nav text color changed to gray

--- src/components/animation/Sphere/shaders/fragmentShader.ts
+++
@@ -1,9 +0,0 @@
-const fragmentShader = `
-  varying vec3 vColor;
-
-  void main() {
-    gl_FragColor = vec4(vColor, 1.0);
-  }
-`
-
-export default fragmentShader
--- src/components/animation/Sphere/shaders/index.ts
+++
@@ -1,5 +0,0 @@
-import vertex from './vertexShader'
-import fragment from './fragmentShader'
-
-export const vertexShader = vertex
-export const fragmentShader = fragment
